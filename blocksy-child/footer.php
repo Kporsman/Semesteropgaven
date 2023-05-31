2e246d7e1255cf40cf38e393b2a9a09e12ab6c13
Kporsman <95682845+Kporsman@users.noreply.github.com>
2023-05-31 22:55:04 +0200

    .

---
+++ blocksy-child/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * @package Blocksy
+ */
+
+blocksy_after_current_template();
+do_action('blocksy:content:bottom');
+
+?>
+	</main>
+    
+	<?php
+		
+	?>
+</div>
+
+<div class="try">
+            <div class="center">
+            <h3>Professionel kæledyrspasning når du ikke selv er hjemme</h3> 
+        </div>
+</div>
+
+
+
+</body>
+</html>
---
+++ blocksy-child/page.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The template for displaying all pages
+
+ *
+ * @package Blocksy
+ */
+
+get_template_part( 'single' );
